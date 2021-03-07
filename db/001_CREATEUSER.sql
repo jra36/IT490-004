43f1fa5ed61760eca2acf93065eb0246ace30df4
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-07 17:28:34 -0500

    Delete db directory

--- db/001_CREATEUSER.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Users(
-		id int not null auto_increment,
-		email varchar(100) not null,
-		password varchar(60) not null,
-		primary key(id),
-		unique(email),
-		created timestamp default current_timestamp,
-		modified timestamp default current_timestamp on update current_timestamp;
-	)
