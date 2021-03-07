cbfbc46bb2704a3c7fc2f69dbbe3a4d2b5655e9b
Ubuntu <ubuntu@ip-172-31-67-177.ec2.internal>
2021-03-07 23:10:33 +0000

    Fix DBConn

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -3,7 +3,7 @@ function getDB(){
 	global $db;
 	if(!isset($db)){
 		//DO NOT COMMIT PRIVATE CREDENTIALS TO A REPOSITORY EVER
-		require_once(__DIR__ . "/../lib/config.php");
+		require_once(__DIR__ . "/../lib/db.php");
 		$conn_string = "mysql:user=$dbuser;dbname=$dbdatabase;charset=utf8mb4";//TODO should pull from config or env variables
 		$db = new PDO($conn_string, $dbusername, $dbpassword);
 	}
