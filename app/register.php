59250556f909ebb45d27af4a85b9e90d6a96b607
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-09 17:55:45 -0500

    Update register.php

--- app/register.php
+++ app/register.php
@@ -14,23 +14,22 @@ session_start();
 if(isset($_POST["submit"])){
   
 	$data = $_POST;
-  
-	if(empty($data["username"]) || !empty($data["password"]) || !empty($data["confirm"])) {
+        if(empty($data["username"]) || !empty($data["password"]) || !empty($data["confirm"])) {
     
-	    die('Please fill in all fields');
+             die('Please fill in all fields');
 	}
   
 	else if {
-    $data["password"] != $data["confirm"];
-    die ("Passwords do not match, please enter again.");
-  }
-    
-   else {
+          $data["password"] != $data["confirm"];
+          die ("Passwords do not match, please enter again.");
+          }
+     
+          else {
       	
-    $username = $_POST["username"];
+          $username = $_POST["username"];
 	  $password = $_POST["password"];
-    $confirm = $_POST["confirm"];
-    }
+          $confirm = $_POST["confirm"];
+          }
       
 	//calls function from MQPublish.inc.php to communicate with MQ
 	$response = login($username, $password);
