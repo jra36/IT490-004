a72b2b628fb979f8e0322884922b0fad7f64ccdb
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-12 15:20:52 -0500

    Update login.php

--- app/login.php
+++ app/login.php
@@ -25,7 +25,7 @@ if(isset($_POST["submit"])){
 	$response = login($username, $password);
 	if($response->status == 200){
 		var_export($response->status, true);
-		header("Location: login.php");
+		header("Location: dashboard.php");
 		exit();
 	}
 	else{
