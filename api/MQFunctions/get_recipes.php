e6d6adeee312b016f6cea26a7203798e0bfde294
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-21 18:51:06 -0400

    Create get_recipes.php

---
+++ api/MQFunctions/get_recipes.php
@@ -0,0 +1,30 @@
+<?php
+function get_recipes($q) {
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?limitLicense=false&offset=0&number=10&query=%24q&includeIngredients=strawberry&minVitaminA=500&minFiber=0&maxPotassium=1000&minSelenium=0",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key: cf2218c96cmsh042b7223a0f13a6p17be8fjsna7f12d6f3964"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
+}
