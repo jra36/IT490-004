b2665b3fff0d394fe07757fb88ab2ed32078b279
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-23 17:35:18 -0400

    Update get_recipes.php

--- api/MQFunctions/get_recipes.php
+++ api/MQFunctions/get_recipes.php
@@ -3,7 +3,7 @@ function get_recipes($q) {
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-	CURLOPT_URL => "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?limitLicense=false&offset=0&number=10&query=$q&includeIngredients=strawberry&minVitaminA=500&minFiber=0&maxPotassium=1000&minSelenium=0",
+	CURLOPT_URL => "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex?limitLicense=false&offset=0&number=10&query=$q&includeIngredients=strawberry",
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_ENCODING => "",
