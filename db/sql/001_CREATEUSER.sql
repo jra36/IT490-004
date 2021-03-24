f16c7e5305f539b725583d0c0f44d46c5a017bd7
Ubuntu <ubuntu@ip-172-31-66-116.ec2.internal>
2021-03-24 20:57:50 +0000

    Updated SQL

--- db/sql/001_CREATEUSER.sql
+++ db/sql/001_CREATEUSER.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS Users(
 		id int not null auto_increment,
 		email varchar(100) not null,
 		password varchar(60) not null,
+		roleid int,
 		primary key(id),
 		unique(email),
 		created timestamp default current_timestamp,
