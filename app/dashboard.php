f81057fbd1e8ab7f5a2c95364d0719d94725e9e6
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-25 16:11:39 -0400

    Update dashboard.php

--- app/dashboard.php
+++ app/dashboard.php
@@ -29,8 +29,8 @@ h1 {
 <b><h1 style="font-size:30px;"> </h></b>
 <form method="POST">
 	<label for="query">Find recipes here:</label><br>
-  	<input type="text" id="query" name="query" size = "40" >
-  	<input type="submit" value="submit" >
+  	<input type="text" name="query" size = "40" >
+  	<button type="submit" name="submit">Search</button>
 </form> 
 </body>
 </html>
