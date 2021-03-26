18bbae366930f7b4038297738037722abb302f47
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-26 17:37:52 -0400

    Update dashboard.php

--- app/dashboard.php
+++ app/dashboard.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+require(__DIR__."/MQPublish.inc.php");
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+if(isset($_POST["submit"]) && isset($_POST["query"]) && !empty($_POST["query"])){
+	
+	
+		$query = $_POST["query"];
+	}
+
+	else {
+		die('Please Enter a Query.');
+	}
+
+	//calls function from MQPublish.inc.php to communicate with MQ
+	$response = get_recipes($query);
+?>
+
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
@@ -31,36 +51,43 @@ h1 {
 	<label for="query">Find recipes here:</label><br>
   	<input type="text" name="query" size = "40" >
   	<button type="submit" name="submit">Search</button>
-</form> 
+</form>
 	
-<?php
-session_start();
-require(__DIR__."/MQPublish.inc.php");
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-
-if(isset($_POST["submit"]) && isset($_POST["query"]) && !empty($_POST["query"])){
+<div id="output"> 	
 	
-	
-		$query = $_POST["query"];
-	}
 
-	else {
-		die('Please Enter a Query.');
-	}
+	<?php  if(isset($response["results"])): ?>
+	<?php foreach($response["results"] as $post): ?>
+		<article>
+			<header>
+				<h3><?php echo $post['title'];?></h3>
+				<h6><?php echo $post['id'];?></h6>
+				<img height="100px" width="100px" src="<?php echo $post['image'];?>"/>
+			</header>
+			<section>
+				<h4>Likes</h4>
+				<p><?php echo $post['likes'];?></p>
+			</section>
+			<section>
+				<h4>Used Ingredient Count</h4>
+				<p><?php echo $post['usedIngredientCount'];?></p>
+			</sections>
+			<footer>
+				<section>
+					<h5>Missed Ingredient Count</h5>
+					<time><?php echo $post['missedIngredientCount'];?></time>
+				</section>
+			
+			</footer>
+			
+		</article>
+	<?php endforeach; ?>
+	<?php else: ?>
+		<div>No Recipes Found.</div>
 
-	//calls function from MQPublish.inc.php to communicate with MQ
-	$response = get_recipes($query);
+	<?php endif; ?>
 
-	
-	if(isset($response["results"]))  {
-	     foreach ($response["results"] as $post) {
-             echo '<h3>' . $post['title'] . '</h3>';
-         }
-	}
-
-     
-?>
+</div>
 </body>
 </html>
 
