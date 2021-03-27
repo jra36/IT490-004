e42190267bbd2bab7493792f664acea0f63f0611
jb695 <77747647+jb695@users.noreply.github.com>
2021-03-26 20:53:43 -0400

    Update admindashboard.php

--- app/admindashboard.php
+++ app/admindashboard.php
@@ -35,7 +35,7 @@ form{margin:auto; border:2px dashed black; padding:20px; width:300px; }
 <div id = "ingredient1"  ><input type = text name= "ingredient1"   > Enter Ingredient 1<br>
 <div id = "ingredient2"  ><input type = text name= "ingredient2"   > Enter Ingredient 2<br>
 <div id = "ingredient3"  ><input type = text name= "ingredient3"   > Enter Ingredient 3<br>
-<!-- <div id = "description"  ><input type = text name= "description"   > Enter Description <br> -->
+<div id = "image"  ><input type = text name= "image"   > Enter URL of Recipe Image<br>
 <div id = "description"  >
 <textarea name =description rows="4" col="50">
 Enter a description of the recipe.
@@ -50,6 +50,8 @@ Enter a description of the recipe.
 var ptrCreate = document.getElementById("Create")
 var ptrSearch = document.getElementById("Search")
 var ptrClear = document.getElementById("Clear")
+
+
 var ptrName = document.getElementById("name") 
 var ptrQuery= document.getElementById("query")
 var ptrID= document.getElementById("id")
@@ -57,11 +59,14 @@ var ptrCalories= document.getElementById("calories")
 var ptrIngredient1 = document.getElementById("ingredient1")
 var ptrIngredient2 = document.getElementById("ingredient2")
 var ptrIngredient3 = document.getElementById("ingredient3")
+var ptrImage = document.getElementById("image")
 var ptrDescription = document.getElementById("description")
+
 ptrCreate.addEventListener("click", F)
 ptrSearch.addEventListener("click", F)
 ptrClear.addEventListener("click", F)
 ptrFavorite.addEventListener("click", F)
+	
 function F(){
 	ptrName.style.display =  "none" 
 	ptrQuery.style.display =  "none"
@@ -70,7 +75,9 @@ function F(){
 	ptrIngredient1.style.display =  "none"
 	ptrIngredient2.style.display =  "none"
 	ptrIngredient3.style.display =  "none"
-    ptrDescription.style.display =  "none"
+        ptrDescription.style.display =  "none"
+	
+	
 	if(this.value == "Create") {
 		ptrName.style.display =  "block"
 		ptrID.style.display =  "block"
@@ -78,7 +85,8 @@ function F(){
 		ptrIngredient1.style.display =  "block"
 		ptrIngredient2.style.display =  "block"
 		ptrIngredient3.style.display =  "block"
-        ptrDescription.style.display = "block"
+		ptrImage.style.display = "block"
+                ptrDescription.style.display = "block"
 	
 	}
 	if(this.value == "Search"){
