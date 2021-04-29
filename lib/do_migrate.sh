b1853671868a2b1728b157ee4d5cb953746ab393
jb695 <77747647+jb695@users.noreply.github.com>
2021-04-29 16:26:08 -0400

    Update do_migrate.sh

--- lib/do_migrate.sh
+++ lib/do_migrate.sh
@@ -1,2 +1,2 @@
 # source folder destination folder
-./implement.sh /path/to/receive /path/to/live
+./implement.sh ubuntu@3.237.32.127:/home/ubuntu/IT490-005/lib/test.txt ubuntu@3.236.220.123:/home/ubuntu
