e8da192ec77ec597fe4fc1439511bbc4ee2990bb
jb695 <77747647+jb695@users.noreply.github.com>
2021-04-29 16:25:17 -0400

    Update migrate.sh

--- lib/migrate.sh
+++ lib/migrate.sh
@@ -1,6 +1,6 @@
 #we can maybe hardcode static ref to host
-host=""
-user=""
+host="3.237.32.127"
+user="ubuntu"
 #get source from arg or default to current dir
 [ -z "$1" ] && source=. || source=$1
 if test -z "$2"
@@ -12,4 +12,4 @@ dest=$2
 echo "Pushing data"
 sudo ./push.sh $source $user $host $dest
 echo"Running remote migrate"
-ssh -i ~/.ssh/id_rsa_migration $user@$host /home/ubuntu/do_migrate.sh
+ssh -i ~/.ssh/id_ed25519 $user@$host /home/ubuntu/do_migrate.sh
