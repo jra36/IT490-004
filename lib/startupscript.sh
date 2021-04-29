0a7454e66a8fe74b2d9a19fa58d6bcea7ee7faac
jb695 <77747647+jb695@users.noreply.github.com>
2021-04-29 17:20:17 -0400

    Update startupscript.sh

--- lib/startupscript.sh
+++ lib/startupscript.sh
@@ -13,3 +13,12 @@ sudo apt upgrade -y
 
 #Install the necessary packages for API VM
 sudo apt install -y php7.4 php7.4-cli php-mbstring php-bcmath composer
+
+#Changes password of Ubuntu User to make SSH'ing easier
+sudo passwd ubuntu
+
+#Opens the file for editing SSH permission
+sudo nano /etc/ssh/sshd_config
+
+
+
