7f46cbc13da073fe0e6a2a28fc9add22b5394521
Ubuntu <ubuntu@ip-172-31-60-119.ec2.internal>
2021-05-01 01:35:40 +0000

    Updated DB setup script.

--- DB_Setup_Simple.sh
+++ DB_Setup_Simple.sh
@@ -10,7 +10,10 @@ sudo apt update -y
 sudo apt upgrade -y
 
 #install composer
-sudo apt install -y composer 
+sudo apt install -y composer
+
+#install bc-math
+sudo apt install -y php-bcmath
 
 #install php and required libs
 sudo apt install -y php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline
