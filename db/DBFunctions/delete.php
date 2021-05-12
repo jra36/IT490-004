dfd5f4c3300524957fd54a1556fdab7e4c3aa112
jb695 <77747647+jb695@users.noreply.github.com>
2021-05-12 17:59:17 -0400

    Update delete.php

--- db/DBFunctions/delete.php
+++ db/DBFunctions/delete.php
@@ -6,12 +6,14 @@ function delete_recipe($id){
 	$result = $stmt->execute([":i"=>$id]);
 	//TODO do proper checking, maybe user doesn't exist
 	if($result){
-		return array("\n\n""message"=>"Successfully deleted recipe.");
+		echo "<br><br>";
+		return array("message"=>"Successfully deleted recipe.");
 	}
 	else{
 		//must return a proper message so that the app can parse it
 		//and display a user friendly message to the user
-		return array("\n\n""message"=>"Failed to delete recipe.");
+		echo "<br><br>";
+		return array("message"=>"Failed to delete recipe.");
 		
 	}
 	
