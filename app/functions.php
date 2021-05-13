c945365aeb255845dab61f9bb8feaf1657ca60fe
jb695 <77747647+jb695@users.noreply.github.com>
2021-05-12 23:21:31 -0400

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -25,7 +25,7 @@ if($choice == "Search")
 		if(isset($response["results"])) {
 			foreach($response["results"] as $post){
 			 echo $post['title'];
-			echo '<img src="$post['image']" height="200px" width="200px">';
+			echo "<img src="$post['image']" height="200px" width="200px">";
 				
 				
 			}
