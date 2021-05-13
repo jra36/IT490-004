302873d6ee48143c368e1253a5903154b78d79b0
jb695 <77747647+jb695@users.noreply.github.com>
2021-05-13 01:29:51 -0400

    Update logout.php

--- app/logout.php
+++ app/logout.php
@@ -1,5 +1,6 @@
 <?
+session_start();
 session_unset();
 session_destroy();
-die(header("Location: http://app-load-balancer-818186467.us-east-2.elb.amazonaws.com/~ubuntu/app/register.php"))
+die(header("Location: http://app-load-balancer-818186467.us-east-2.elb.amazonaws.com/~ubuntu/app/register.php"));
 ?>
